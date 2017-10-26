cdd621eb4faa1ad6c9a4011a374c454b5a6b738b
chongchen <cc.chong.chen@gmail.com>
2017-10-26 22:17:07 +0800

    add Uri strings

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,5 +1,6 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -10,7 +11,16 @@ public final class PetContract {
 
     private PetContract() {}
 
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_PETS = "pets";
+
     public static final class PetEntry implements BaseColumns {
+
+        // The content URI to access the pet data in the provider
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
         // name of database table for pets
         public final static String TABLE_NAME = "pets";
